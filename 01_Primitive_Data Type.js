9d7053beac41d36c3c042ffab564d24f75c651e2
Asit Prusty <31497382+asit3160@users.noreply.github.com>
2024-01-11 01:10:53 +0530

    Add files via upload

---
+++ 01_Primitive_Data Type.js
@@ -0,0 +1,29 @@
+/*
+1) Javascript File Name: 01_learn.js
+ 
+    Objective: Learn Primitive Data Types
+ 
+    Create the following variables using var and check their typeOf
+ 
+    a) firstName
+    b) companyName
+    c) mobileNumber
+    d) isAutomation
+    e) hasPlaywright (do not assign)
+ 
+    print and confirm the values and data types
+*/
+
+const { log } = require("console");
+
+var firstName="Asit";
+var companyName="Happiestminds";
+var mobileNumber="9087443160";
+var isAutomation= false;
+var hasPlaywright;
+
+console.log(firstName);
+console.log(companyName);
+console.log(mobileNumber);
+console.log(isAutomation);
+console.log(hasPlaywright);
